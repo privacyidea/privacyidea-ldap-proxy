6e83202426e4c81deb718bf1883ef07b182d0211
Friedrich Weber <friedrich.weber@netknights.it>
2017-06-19 18:16:49 +0200

    Add simple login premable detection mechanism
    Working on #13

---
+++ pi_ldapproxy/realmmapping.py
@@ -0,0 +1,52 @@
+from ldaptor.protocols.pureldap import LDAPFilter_and, LDAPFilter_or, LDAPFilter_equalityMatch, LDAPSearchRequest, \
+    LDAPSearchResultEntry
+from twisted.logger import Logger
+
+log = Logger()
+
+
+def find_app_marker(filter, attribute='objectclass', value_prefix='App-'):
+    """
+    Given an ldaptor filter, try to extract an app marker, i.e. find
+    a marker such that the filter contains an expression (<attribute>=<value_prefix><marker>),
+    e.g. (objectclass=App-ownCloud).
+    It may be nested in &() and |() expressions.
+    :param filter: ldaptor filter
+    :param attribute: attribute name whose value contains the app marker
+    :param value_prefix: prefix of the app marker
+    :return: None or an app marker (a string)
+    """
+    if isinstance(filter, LDAPFilter_and) or isinstance(filter, LDAPFilter_or):
+        # recursively search and/or expressions
+        for subfilter in filter:
+            app_marker = find_app_marker(subfilter, attribute, value_prefix)
+            if app_marker:
+                return app_marker
+    elif isinstance(filter, LDAPFilter_equalityMatch):
+        # check attribute name and value prefix
+        if filter.attributeDesc.value == attribute:
+            value = filter.assertionValue.value
+            if value.startswith(value_prefix):
+                return value[len(value_prefix):]
+    return None
+
+
+def detect_login_preamble(request, response, attribute='objectclass', value_prefix='App-'):
+    """
+    Determine whether the request/response pair constitutes a login preamble.
+    If it does, return the login DN and the app marker.
+    :param request: LDAP request
+    :param response: LDAP response
+    :param attribute: see ``find_app_marker``
+    :param value_prefix: see ``find_app_marker``
+    :return: A tuple ``(DN, app marker)`` or None
+    """
+    if isinstance(request, LDAPSearchRequest) and request.filter:
+        # TODO: Check base dn?
+        marker = find_app_marker(request.filter, attribute, value_prefix)
+        # TODO: This will be called multiple times for the same search request!
+        # i.e. we do not notice if the response has >1 entries
+        if marker is not None and isinstance(response, LDAPSearchResultEntry):
+            log.info('Detected login preamble: {!r} ({!r})'.format(response.objectName, marker))
+            return (response.objectName, marker)
+    return None
---
+++ pi_ldapproxy/test/test_realmmapping.py
@@ -0,0 +1,44 @@
+import twisted
+from ldaptor.ldapfilter import parseFilter
+from ldaptor.protocols import pureldap
+
+from pi_ldapproxy.realmmapping import find_app_marker, detect_login_preamble
+
+
+class TestRealmMapping(twisted.trial.unittest.TestCase):
+    def test_find_app_marker(self):
+        filter = parseFilter('(&(|(objectclass=person)(objectclass=App-someApp))(cn=user123))')
+        self.assertEqual(find_app_marker(filter), 'someApp')
+
+        filter = parseFilter('(&(|(objectclass=person)(someOtherAttribute=App-someApp))(cn=user123))')
+        self.assertIsNone(find_app_marker(filter))
+        self.assertEqual(find_app_marker(filter, attribute='someOtherAttribute'), 'someApp')
+
+        filter = parseFilter('(&(|(objectclass=person)(someOtherAttribute=Prefix-someApp))(cn=user123))')
+        self.assertEqual(find_app_marker(filter, attribute='someOtherAttribute', value_prefix='Prefix-'), 'someApp')
+
+        filter = parseFilter('(&(|(objectclass=person))(cn=user123))')
+        self.assertIsNone(find_app_marker(filter))
+
+    def test_detect_login_preamble(self):
+        filter = parseFilter('(&(|(objectclass=person)(objectclass=App-someApp))(cn=user123))')
+        request = pureldap.LDAPSearchRequest(baseObject='cn=users,dc=test,dc=local',
+                                  scope=pureldap.LDAP_SCOPE_wholeSubtree, derefAliases=0,
+                                  sizeLimit=0, timeLimit=0, typesOnly=0,
+                                  filter=filter,
+                                  attributes=())
+        dn = 'cn=user123,cn=users,dc=test,dc=local'
+        response = pureldap.LDAPSearchResultEntry(dn, [('cn', ['user123'])])
+        self.assertEqual(detect_login_preamble(request, response), (dn, 'someApp'))
+
+        self.assertIsNone(detect_login_preamble(request, pureldap.LDAPSearchResultDone(0)))
+
+        filter = parseFilter('(&(|(objectclass=person)(someAttribute=Foo-someApp))(cn=user123))')
+        request = pureldap.LDAPSearchRequest(baseObject='cn=users,dc=test,dc=local',
+                                             scope=pureldap.LDAP_SCOPE_wholeSubtree, derefAliases=0,
+                                             sizeLimit=0, timeLimit=0, typesOnly=0,
+                                             filter=filter,
+                                             attributes=())
+        dn = 'cn=user123,cn=users,dc=test,dc=local'
+        response = pureldap.LDAPSearchResultEntry(dn, [('cn', ['user123'])])
+        self.assertEqual(detect_login_preamble(request, response, 'someAttribute', 'Foo-'), (dn, 'someApp'))
