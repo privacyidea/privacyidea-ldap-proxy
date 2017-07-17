83ce8534ea61271fa56f06c223344a5d8a7f9030
Friedrich Weber <friedrich.weber@netknights.it>
2017-07-17 17:02:12 +0200

    Set version 0.4

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ info:
 	@echo "make builddeb     - build .deb file locally on ubuntu 14.04LTS!"
 	
 #VERSION=1.3~dev5
-SHORT_VERSION=0.3~dev0
+SHORT_VERSION=0.4~dev0
 #SHORT_VERSION=2.10~dev7
 VERSION_JESSIE=${SHORT_VERSION}
 VERSION=${SHORT_VERSION}
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pi-ldapproxy',
-      version='0.3',
+      version='0.4',
       description='privacyIDEA LDAP Proxy based on Twisted',
       packages=['pi_ldapproxy', 'pi_ldapproxy.test', 'twisted.plugins'],
       )
