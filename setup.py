d63bff9bf9ae07718b4c51f9e43fc2dedfe20f97
Friedrich Weber <friedrich.weber@netknights.it>
2017-08-02 12:54:52 +0200

    Set version 0.4.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pi-ldapproxy',
-      version='0.4',
+      version='0.4.1',
       description='privacyIDEA LDAP Proxy based on Twisted',
       packages=['pi_ldapproxy', 'pi_ldapproxy.test', 'twisted.plugins'],
       )
