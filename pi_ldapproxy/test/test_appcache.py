1800950a2831a617b5648649d4b477452d8ef587
Friedrich Weber <friedrich.weber@netknights.it>
2017-12-12 15:41:11 +0100

    Add tests for case insensitive DN matching
    Working on #38

--- pi_ldapproxy/test/test_appcache.py
+++ pi_ldapproxy/test/test_appcache.py
@@ -5,6 +5,7 @@ from twisted.internet import task
 from pi_ldapproxy.appcache import AppCache
 
 DN1 = 'cn=test,cn=users,dc=test,dc=intranet'
+DN1_OTHERCASE = 'cn=test,cn=Users,dc=test,DC=IntraNET'
 DN2 = 'cn=other,cn=users,dc=test,dc=intranet'
 DN3 = 'cn=someone,cn=users,dc=test,dc=intranet'
 MARKER1 = 'marker1'
@@ -20,6 +21,7 @@ class TestAppCache(twisted.trial.unittest.TestCase):
         cache.add_to_cache(DN1, MARKER2)
         cache.add_to_cache(DN2, MARKER1)
         self.assertEqual(cache.get_cached_marker(DN1), MARKER2) # is overwritten!
+        self.assertEqual(cache.get_cached_marker(DN1_OTHERCASE), None)
         self.assertEqual(cache.get_cached_marker(DN2), MARKER1)
         self.assertEqual(cache.get_cached_marker(DN3), None)
 
@@ -41,10 +43,35 @@ class TestAppCache(twisted.trial.unittest.TestCase):
         # Remove (DN1, MARKER1) -- no effect.
         cache.remove_from_cache(DN1, MARKER1)
         self.assertEqual(cache.get_cached_marker(DN1), MARKER2)
+        # Remove (DN1_OTHERCASE, MAKER2) -- no effect.
+        cache.remove_from_cache(DN1_OTHERCASE, MARKER2)
+        self.assertEqual(cache.get_cached_marker(DN1), MARKER2)
         # Remove (DN1, MARKER2) -- removed!
         cache.remove_from_cache(DN1, MARKER2)
         self.assertEqual(cache.get_cached_marker(DN1), None)
         self.assertEqual(cache.get_cached_marker(DN2), None)
+
+    def test_case_insensitive(self):
+        clock = task.Clock()
+        cache = AppCache(2, True)
+        cache.callLater = clock.callLater
+        # Add and wait a second, it should still be there (also in another case)
+        cache.add_to_cache(DN1, MARKER1)
+        clock.advance(1)
+        self.assertEqual(cache.get_cached_marker(DN1_OTHERCASE), MARKER1)
+        # Wait another two seconds, it should not be there anymore
+        clock.advance(2)
+        self.assertEqual(cache.get_cached_marker(DN1_OTHERCASE), None)
+        # Add and remove manually
+        cache.add_to_cache(DN1, MARKER1)
+        self.assertEqual(cache.get_cached_marker(DN1.upper()), MARKER1)
+        self.assertEqual(cache.get_cached_marker(DN1_OTHERCASE.upper()), MARKER1)
+        clock.advance(1)
+        self.assertEqual(cache.get_cached_marker(DN1.upper()), MARKER1)
+        self.assertEqual(cache.get_cached_marker(DN1_OTHERCASE.upper()), MARKER1)
+        cache.remove_from_cache(DN1_OTHERCASE, MARKER1)
+        self.assertEqual(cache.get_cached_marker(DN1.upper()), None)
+        self.assertEqual(cache.get_cached_marker(DN1_OTHERCASE.upper()), None)
 
     def test_automatic_removal(self):
         clock = task.Clock()
--- pi_ldapproxy/test/test_proxy_app_cache.py
+++ pi_ldapproxy/test/test_proxy_app_cache.py
@@ -135,6 +135,37 @@ class TestProxyUserBind(ProxyTestCase):
         time.sleep(1) # to clean the reactor
 
     @defer.inlineCallbacks
+    def test_realm_mapping_fails_case_sensitive(self):
+        marker = 'markerSecret'
+        password = 'secret'
+        service_dn = 'uid=passthrough,cn=users,dc=test,dc=local'
+        dn = 'uid=Hugo,cn=users,dc=test,DC=LOCAL'
+        server, client = self.create_server_and_client([
+            pureldap.LDAPBindResponse(resultCode=0), # for service account
+        ], [
+            pureldap.LDAPSearchResultEntry(dn, [('someattr', ['somevalue'])]),
+            pureldap.LDAPSearchResultDone(ldaperrors.Success.resultCode),
+        ])
+        yield client.bind(service_dn, 'service-secret')
+        # Assert that Proxy<->Backend uses the correct credentials
+        server.client.assertSent(
+            pureldap.LDAPBindRequest(dn=service_dn, auth='service-secret'),
+        )
+        # Perform a simple search in the context of the service account
+        entry = LDAPEntry(client, dn)
+        r = yield entry.search('(|(objectClass=*)(objectclass=App-%s))' % marker, scope=pureldap.LDAP_SCOPE_baseObject)
+        # sleep half a second and then try to bind as hugo
+        time.sleep(0.5)
+        server2, client2 = self.create_server_and_client([
+            pureldap.LDAPBindResponse(resultCode=0),  # for service account (successful hugo bind)
+        ])
+        d = client2.bind(dn.lower(), password) # this will fail because the DN has differing case
+        yield self.assertFailure(d, ldaperrors.LDAPInvalidCredentials)
+        self.assertEqual(self.privacyidea.authentication_requests,
+                         [])
+        time.sleep(1) # to clean the reactor
+
+    @defer.inlineCallbacks
     def test_realm_mapping_fails_waiting_too_long(self):
         service_dn = 'uid=passthrough,cn=users,dc=test,dc=local'
         dn = 'uid=hugo,cn=users,dc=test,dc=local'
@@ -198,4 +229,59 @@ class TestProxyUserBind(ProxyTestCase):
         entry2 = LDAPEntry(client2, dn)
         r = yield entry2.search('(|(objectClass=*)(objectcLAsS=App-markerOfficial))', scope=pureldap.LDAP_SCOPE_baseObject)
         self.assertTrue(server.factory.app_cache.get_cached_marker(dn) in ('markerSecret', None))
+        time.sleep(1) # to clean the reactor
+
+class TestProxyUserBindCaseInsensitive(ProxyTestCase):
+    privacyidea_credentials = {
+        'hugo@realmSecret': 'secret',
+        'hugo@realmOfficial': 'password',
+    }
+
+    additional_config = {
+        'ldap-proxy': {
+            'bind-service-account': True,
+            'allow-search': True,
+        },
+        'realm-mapping': {
+            'strategy': 'app-cache',
+            'mappings': {
+                'markerSecret': 'realmSecret',
+                'markerOfficial': 'realmOfficial',
+            },
+        },
+        'app-cache': {
+            'enabled': True,
+            'timeout': 1,
+            'case-insensitive': True,
+        }
+    }
+
+    @defer.inlineCallbacks
+    def test_realm_mapping_succeeds_case_sensitive(self):
+        marker = 'markerSecret'
+        password = 'secret'
+        service_dn = 'uid=passthrough,cn=users,dc=test,dc=local'
+        dn = 'uid=Hugo,cn=users,dc=test,DC=LOCAL'
+        server, client = self.create_server_and_client([
+            pureldap.LDAPBindResponse(resultCode=0), # for service account
+        ], [
+            pureldap.LDAPSearchResultEntry(dn, [('someattr', ['somevalue'])]),
+            pureldap.LDAPSearchResultDone(ldaperrors.Success.resultCode),
+        ])
+        yield client.bind(service_dn, 'service-secret')
+        # Assert that Proxy<->Backend uses the correct credentials
+        server.client.assertSent(
+            pureldap.LDAPBindRequest(dn=service_dn, auth='service-secret'),
+        )
+        # Perform a simple search in the context of the service account
+        entry = LDAPEntry(client, dn)
+        r = yield entry.search('(|(objectClass=*)(objectclass=App-%s))' % marker, scope=pureldap.LDAP_SCOPE_baseObject)
+        # sleep half a second and then try to bind as hugo
+        time.sleep(0.5)
+        server2, client2 = self.create_server_and_client([
+            pureldap.LDAPBindResponse(resultCode=0),  # for service account (successful hugo bind)
+        ])
+        yield client2.bind(dn.lower(), password) # this will work even though the DN has differing case
+        self.assertEqual(self.privacyidea.authentication_requests,
+                         [('hugo', 'realmSecret', password, True)])
         time.sleep(1) # to clean the reactor
