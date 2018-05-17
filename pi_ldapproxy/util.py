94e708dd3ab6d9e9f2d820c5a5adfab0c7a080d9
Friedrich Weber <friedrich.weber@netknights.it>
2018-05-17 20:08:37 +0200

    Improve comments

--- pi_ldapproxy/util.py
+++ pi_ldapproxy/util.py
@@ -8,12 +8,19 @@ from zope.interface import implementer
 
 @implementer(IOpenSSLClientConnectionCreator)
 class DisabledVerificationClientTLSOptions(ClientTLSOptions):
+    """
+    ClientTLSOptions replacement that does not validate the hostname certificate at all, i.e.
+    neither checks if the certificate matches the hostname nor the certificate's trust chain.
+    """
     def _identityVerifyingInfoCallback(self, connection, where, ret):
         """
-        Do not check the remote hostname
+        In case *where* indicates that the SSL handshake has been done,
+        this does nothing (as opposed to ClientTLSOptions._identityVerifyingInfoCallback,
+        which would validate the certificate). In all other cases,
+        the superclass method is called.
         """
         if where & SSL_CB_HANDSHAKE_DONE:
-            # ClientTLSOptions._identityVerifyingInfoCallback will validate the certificate
+            # ClientTLSOptions._identityVerifyingInfoCallback would validate the certificate
             # in that case. Instead, we just do nothing.
             pass
         else:
